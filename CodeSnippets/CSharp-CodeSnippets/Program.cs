28bc1d7e1e010b414b1cff1db466612c485d84b2
Blaine <blaine91787@gmail.com>
2019-10-21 17:59:55 -0500

    First commit, adding Directional Statistics class

---
+++ CodeSnippets/CSharp-CodeSnippets/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_CodeSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.DirectionalStatistics.ExecuteTests();
+        }
+    }
+}
